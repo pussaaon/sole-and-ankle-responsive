9ddec5368e16f28b82f2d9c76a1edc591b8827a0
Lauv <pussaaon@gmail.com>
2023-12-09 11:13:16 +0700

    Exercise 5: Fluid desktop navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -73,7 +73,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem , 3.5rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndBelow} {
